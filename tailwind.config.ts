e3ae7af8389262fc7d38daf78b541a41ab13d7c6
sangpm99 <sangpm72@wru.vn>
2025-07-05 15:17:09 +0700

    Updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,13 @@
-import tailwindcssLogical from 'tailwindcss-logical'
-import type { Config } from 'tailwindcss'
+import tailwindcssLogical from "tailwindcss-logical"
+import type { Config } from "tailwindcss"
 
-import tailwindPlugin from './src/@core/tailwind/plugin'
+import tailwindPlugin from "./src/@core/tailwind/plugin"
 
 const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,css}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx,css}"],
   corePlugins: {
     preflight: false
   },
-  important: '#__next',
   plugins: [tailwindcssLogical, tailwindPlugin],
   theme: {
     extend: {}
