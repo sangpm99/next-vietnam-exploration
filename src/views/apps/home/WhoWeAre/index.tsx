9b463cf3056e03cc264a081bedadc3ca96d55dc6
sangpm99 <sangpm72@wru.vn>
2025-07-07 08:44:39 +0700

    Updated

--- src/views/apps/home/WhoWeAre/index.tsx
+++ src/views/apps/home/WhoWeAre/index.tsx
@@ -1,5 +1,6 @@
 import { Row, Col, Button } from "antd"
 import { ArrowRightOutlined } from "@ant-design/icons"
+import Image from "next/image"
 
 export default function WhoWeAre() {
   return (
@@ -27,6 +28,106 @@ export default function WhoWeAre() {
               </Button>
             </div>
           </div>
+        </Col>
+
+        <Col span={24}>
+          <div className='my-20'></div>
+        </Col>
+
+        <Col span={24}>
+          <Row>
+            <Col span={6}>
+              <Image
+                src='/images/vietnam-map.png'
+                alt='Viet Nam Map'
+                width={0}
+                height={0}
+                sizes='100vw'
+                style={{ width: "100%", height: "auto" }}
+                className='cursor-pointer'
+                unoptimized
+              ></Image>
+            </Col>
+            <Col span={18} className='ps-20'>
+              <h4 className='text-3xl'>
+                Discover Vietnam <span className='text-primary'>- A Land of Timeless Charm</span>
+              </h4>
+              <p className='text-lg mt-5'>
+                From the bustling streets of Hanoi to the tranquil waters of Ha Long Bay, Vietnam offers a unique blend
+                of ancient traditions, breathtaking landscapes, and vibrant culture. Whether you're seeking adventure in
+                the mountains of Sapa, relaxing on the beaches of Da Nang, or exploring the historic towns of Hoi An and
+                Hue, Vietnam welcomes you with warm hospitality and unforgettable experiences. Let every journey here be
+                a story worth telling.
+              </p>
+
+              <Row gutter={[16, 16]}>
+                <Col span={8}>
+                  <Image
+                    src='/images/image-2.jpg'
+                    alt='HCM City'
+                    width={0}
+                    height={0}
+                    sizes='100vw'
+                    style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                    className='cursor-pointer rounded'
+                    unoptimized
+                  ></Image>
+                </Col>
+                <Col span={16}>
+                  <Row gutter={[16, 16]}>
+                    <Col span={12}>
+                      <Image
+                        src='/images/image-3.jpg'
+                        alt='Ha Long Bay'
+                        width={0}
+                        height={0}
+                        sizes='100vw'
+                        style={{ width: "100%", height: "auto" }}
+                        className='cursor-pointer rounded'
+                        unoptimized
+                      ></Image>
+                    </Col>
+                    <Col span={12}>
+                      <Image
+                        src='/images/image-4.jpg'
+                        alt='Sea of Da Nang'
+                        width={0}
+                        height={0}
+                        sizes='100vw'
+                        style={{ width: "100%", height: "auto" }}
+                        className='cursor-pointer rounded'
+                        unoptimized
+                      ></Image>
+                    </Col>
+                    <Col span={12}>
+                      <Image
+                        src='/images/image-5.jpg'
+                        alt='Hue City'
+                        width={0}
+                        height={0}
+                        sizes='100vw'
+                        style={{ width: "100%", height: "auto" }}
+                        className='cursor-pointer rounded'
+                        unoptimized
+                      ></Image>
+                    </Col>
+                    <Col span={12}>
+                      <Image
+                        src='/images/image-6.jpg'
+                        alt='Mountains'
+                        width={0}
+                        height={0}
+                        sizes='100vw'
+                        style={{ width: "100%", height: "auto" }}
+                        className='cursor-pointer rounded'
+                        unoptimized
+                      ></Image>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </div>
